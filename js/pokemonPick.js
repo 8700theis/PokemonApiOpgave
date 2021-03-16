44a63b4b94eeaaaf537faf83a6b644a112eafb41
8700theis <57965180+8700theis@users.noreply.github.com>
2021-03-16 19:41:08 +0100

    Trainer name and Pokémon data fetched
    Lavede en function til at indsætte det indtastede trainer name fra første page til anden page.
    
    Jeg lavede en function til at fetche ALT data jeg skal bruge fremadrettet i projectet.

--- js/pokemonPick.js
+++ js/pokemonPick.js
@@ -1,3 +1,49 @@
-const trainerName = localStorage.getItem("trainer-name");
+const insetTrainerName = () => {
+    const trainerName = localStorage.getItem("trainer-name");
+    document.querySelector(".content-team-heading").innerText = `${trainerName}'s Team`;
+}
 
-console.log(trainerName);
+function getPokemon(name, moveOne, moveTwo, moveThree, moveFour) {
+    var thisPokemon = {
+        name: name,
+        lvl: 50,
+        imgSprite: "",
+        hpBS: 0,
+        attBS: 0,
+        defBS: 0,
+        moves: [{ moveName: "", movePower: 0 }, { moveName: "", movePower: 0 }, { moveName: "", movePower: 0 }, { moveName: "", movePower: 0 }]
+    };
+
+    let movesUrlList = [];
+
+    fetch(`https://pokeapi.co/api/v2/pokemon/${name}`)
+        .then((response) => response.json())
+        .then((data) => {
+            thisPokemon.imgSprite = data.sprites.front_default;
+            thisPokemon.hpBS = data.stats[0].base_stat;
+            thisPokemon.attBS = data.stats[1].base_stat;
+            thisPokemon.defBS = data.stats[2].base_stat;
+            thisPokemon.moves[0].moveName = data.moves[moveOne].move.name;
+            thisPokemon.moves[1].moveName = data.moves[moveTwo].move.name;
+            thisPokemon.moves[2].moveName = data.moves[moveThree].move.name;
+            thisPokemon.moves[3].moveName = data.moves[moveFour].move.name;
+            movesUrlList[0] = data.moves[moveOne].move.url;
+            movesUrlList[1] = data.moves[moveTwo].move.url;
+            movesUrlList[2] = data.moves[moveThree].move.url;
+            movesUrlList[3] = data.moves[moveFour].move.url;
+
+            for (let i = 0; i < movesUrlList.length; i++) {
+                fetch(movesUrlList[i])
+                    .then((moveResponse) => moveResponse.json())
+                    .then((moveData) => {
+                        thisPokemon.moves[i].movePower = moveData.power;
+                    })
+            }
+        });
+    console.log(thisPokemon);
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+    insetTrainerName();
+    getPokemon("charizard", 0, 1, 2, 37);
+});
